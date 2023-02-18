e20d178c705ab62f81b502b1f5146dd718ed858a
Miriam Fiandanese <mmiy55@gmail.com>
2023-02-19 02:36:21 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :game, only: [:index, :show]
+  resources :board, only: [:index, :show, :destroy] do
+    resources :pin, only: [:new, :create, :destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
